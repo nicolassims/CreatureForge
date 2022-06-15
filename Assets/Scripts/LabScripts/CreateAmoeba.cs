9a3a035a4a6cc2c6b10e86e9639aafca9ca74c1a
nicolassims <sableye.nico@gmail.com>
2022-06-15 11:19:04 -0500

    Console now prints out fully.

--- Assets/Scripts/LabScripts/CreateAmoeba.cs
+++ Assets/Scripts/LabScripts/CreateAmoeba.cs
@@ -1,14 +1,40 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 
 public class CreateAmoeba : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public GameObject ConsoleObj;
+    private TextMeshProUGUI Console;
 
+    // Start is called before the first frame update
+    async void Start()
+    {
+        Console = ConsoleObj.GetComponent<TextMeshProUGUI>();
+        //Console.text = "";
+
+        await PrintMessage("This is a message, for testing purposes.");
+
+
+        Debug.Log("finished printing");
+    }
+
+    private async Task PrintMessage(string msg)
+    {
+        while (Console.text != "")
+        {
+            Console.text = Console.text.Remove(Console.text.Length - 1);
+            await Task.Delay(50);
+        }
+
+        while (Console.text != msg)
+        {
+            Console.text += msg[Console.text.Length];
+            await Task.Delay(50);
+        }
     }
 
     // Update is called once per frame
