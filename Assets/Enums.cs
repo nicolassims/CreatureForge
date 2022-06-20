b60f94266d997f4a43b6317bd0867990ee62c3ee
nicolassims <sableye.nico@gmail.com>
2022-06-19 22:50:31 -0500

    Created enums file.
    Currently contains BodyResources & PerceptionType.

---
+++ Assets/Enums.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BodyResources
+{
+    Air,
+    Blood,
+    Plasma,
+    Sunlight,
+    Ectoplasm,
+    Fire,
+    Venom,
+    Poison,
+    Nutrients
+}
+
+public enum PerceptionType
+{
+    LightVisible,
+    LightInfrared,
+    LightUltraviolet,
+    SoundAudible,
+    SoundHiFreq,
+    SoundLowFreq,
+    VibrationAir,
+    VibrationGround,
+    Touch,
+    Magnetism
+}
+
+
+/*public class Enums : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/
